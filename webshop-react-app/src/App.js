2e9e50e9074143c13a81409440b90397e64809ff
Jesper Andersson <87135834+Jespris@users.noreply.github.com>
2023-07-25 19:45:42 +0300

    Started with basic layout and functionality, shop items and cart, BUG WITH ADD ITEMS TO CART

--- webshop-react-app/src/App.js
+++ webshop-react-app/src/App.js
@@ -1,10 +1,55 @@
 import './App.css';
+import ShopItemContainer from './components/ShopItemContainer'
+import ShopItem from './components/ShopItem'
+import TopBarContainer from './components/TopBarContainer'
+import {useState} from "react";
 
 function App() {
+    let items;
+    const [shopItems, setShopItems] = useState([
+        ["Mattress", "An old used, stinky, stained mattress. 180cm (WARNING: VERY HEAVY)", "20€"],
+        ["Flatscreen TV", "TV with some pixels broken, and the backlight is uneven", "35€"]
+    ])
+
+    const [cartItems, setCartItems] = useState([])
+
+
+
+    const deleteCartItemHandler = (itemID) => {
+        setCartItems(prevState => [
+            ...prevState.slice(0, itemID),
+            ...prevState.slice(itemID + 1)
+        ])
+    }
+
+    const deleteCartHandler = () => {
+        setCartItems([])
+    }
+
+    const addCartItem = (name, price) => {
+        setCartItems(prevState => [...prevState, [name, price]])
+    }
+
+    const addShopItemHandler = (item) => {
+        setShopItems(prevState => [...prevState, item])
+    }
+
+    items = shopItems.map((value, index) => (
+        <ShopItem key={index} name={value[0]} description={value[1]} price={value[2]} addCartItemHandler={addCartItem}></ShopItem>
+    ))
+
   return (
-    <div>
-      hello world!
-    </div>
+      <div style={{margin: '10px'}}>
+          <h1 style={{textAlign: 'center'}}>
+                  Welcome to my shop!
+          </h1>
+          <div>
+              <TopBarContainer cartItems={cartItems} deleteCartItemHandler={deleteCartItemHandler} deleteCartHandler={deleteCartHandler}></TopBarContainer>
+          </div>
+          <div className="App">
+                <ShopItemContainer items={items}></ShopItemContainer>
+          </div>
+      </div>
   );
 }
 
