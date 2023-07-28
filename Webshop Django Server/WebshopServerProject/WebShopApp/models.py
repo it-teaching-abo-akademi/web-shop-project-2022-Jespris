361e30da5c0d95ef7c4f2c8aec805429a0254c47
Jesper Andersson <87135834+Jespris@users.noreply.github.com>
2023-07-28 13:02:47 +0300

    Refactored some code, improved cart and item info

--- Webshop Django Server/WebshopServerProject/WebShopApp/models.py
+++ Webshop Django Server/WebshopServerProject/WebShopApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,4 +8,5 @@ class ShopItem(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=9, decimal_places=2)
+    username = models.CharField(max_length=50, default="default user")
     date = models.DateTimeField(auto_now_add=True)
--- webshop-react-app/src/App.js
+++ webshop-react-app/src/App.js
@@ -6,12 +6,25 @@ import InputForm from "./components/inputForm";
 import {useState, useEffect} from "react";
 
 function App() {
-    const SHOP_ITEM_API_URL = 'http://127.0.0.1:8000/api/v1/shopItems/';
-    const POPULATE_USERS = 'http://127.0.0.1:8000/api/v1/auth/populateUserDB/';
-    const POPULATE_ITEMS = 'http://127.0.0.1:8000/api/v1/shopItems/populateDB/'
+
+    class ItemInShop {
+        constructor(name, description, price, username, date) {
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.username = username;
+            this.date = date;
+        }
+    }
+
+    const SHOP_ITEM_API_URL = 'http://localhost:8000/api/v1/shopItems/';
+    const POPULATE_USERS = 'http://localhost:8000/api/v1/auth/populateUserDB/';
+    const POPULATE_ITEMS = 'http://localhost:8000/api/v1/shopItems/populateDB/';
+    const GET_ALL_USERS = 'http://localhost:8000/api/v1/auth/users/';
     let items;
     const [shopItems, setShopItems] = useState([])
     const [cartItems, setCartItems] = useState([])
+
 
 
     const deleteCartItemHandler = (itemID) => {
@@ -34,17 +47,17 @@ function App() {
     }
 
     items = shopItems.map((value, index) => (
-        <ShopItem key={index} name={value[0]} description={value[1]} price={value[2]} addCartItemHandler={addCartItem}></ShopItem>
+        <ShopItem key={index} shopItem={value} addCartItemHandler={addCartItem}></ShopItem>
     ))
 
-    const apiFetchPost = (name, description, price) => {
-        console.log("Posting shop item '", name, "' to API")
+    const apiFetchPost = (shopItem) => {
+        console.log("Posting shop item '", shopItem.name, "' to API")
         fetch(SHOP_ITEM_API_URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({name: name, description: description, price: price})
+            body: JSON.stringify({name: shopItem.name, description: shopItem.description, price: shopItem.price, username: shopItem.username})
         }).then(
             response => {
                 if(!response.ok) {
@@ -66,61 +79,105 @@ function App() {
         return await response.json()
     }
 
+    const APIFetchUsers = async () => {
+        const response = await fetch(GET_ALL_USERS);
+        if (!response.ok){
+            throw new Error("API fetch error: " + response.statusCode)
+        }
+        return await response.json()
+    }
+
     const APIFetchShopItems = () => {
         console.log("Fetching shop items...")
         APIFetch().then(data => setShopItems(
             (prev => data.results.map(
-                p => [p.name, p.description, p.price]))))
+                p => new ItemInShop(p.name, p.description, p.price, p.username, p.date)))))
             .catch(err => console.log("Error: ", err))
         console.log("DONE fetching shop items!")
     }
 
-    const PopulateDB = () => {
-        console.log("Populating database with new users and their shop items")
-        // API Call to delete items in database
-        fetch(POPULATE_ITEMS, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({number: 0, delete_existing: true})
-        }).then(
-            response => {
-                if (!response.ok){
-                    throw new Error("Error deleting items from api call: " + response.statusCode)
-                }
-                return response.json()
-            }
-        )
-        // Add users
-        fetch(POPULATE_USERS, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({number: 6})
-        }).then(
-            response => {
-                if(!response.ok) {
-                    throw new Error("API posting error: " + response.statusCode);
-                }
-                return response.json();
-            }
-        ).then(data => {
-            console.log("DATA: ", data)
-        }).catch(err => console.log("Error: ", err))
+    const deleteItemsFromApi = () => {
+      console.log("Deleting items from the API");
+      return fetch(POPULATE_ITEMS, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number: 0, delete_existing: true }),
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error("Error deleting items from API call: " + response.statusCode);
+        }
+        return response.json();
+      });
+    };
 
-        // TODO: get newly created users and create shop items from them!
+    const addUsersToApi = () => {
+      console.log("Adding users to the API");
+      return fetch(POPULATE_USERS, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number: 6 }),
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error("API posting error: " + response.statusCode);
+        }
+        return response.json();
+      });
+    };
+
+    const addShopItemsToApi = (user) => {
+      console.log(`Adding shop items for user ${user.username}`);
+      return fetch(POPULATE_ITEMS, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number: 10, username: user.username, delete_existing: false }),
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error("Error adding items from API call: " + response.statusCode);
+        }
+        return response.json();
+      });
+    };
+
+    const PopulateDB = () => {
+      console.log("Populating database with new users and their shop items");
+
+      deleteItemsFromApi()
+        .then(() => addUsersToApi())
+        .then(data => {
+          console.log("DATA: ", data);
+          return APIFetchUsers();
+        })
+        .then(users => {
+          console.log("USER DATA: ", users);
+          users = [...users.slice(0, 3)];
+          return Promise.all(users.map(user => addShopItemsToApi(user)));
+        })
+        .then(() => refreshPage())
+        .catch(err => console.log("Error: ", err));
+    };
+
+    const refreshPage = () => {
+        console.log("App changed");
+        APIFetch().then(data => {
+            if (data && Array.isArray(data.results)) {
+                setShopItems(
+                prevState => data.results.map(
+                    p => new ItemInShop(p.name, p.description, p.price, p.username, p.date)
+                    )
+                )
+            }
+        }).catch(err => console.log("Error: ", err))
     }
 
     // do whenever refresh
     useEffect(() => {
-        console.log("App changed");
-        APIFetch().then(data => setShopItems(
-            prevState => data.results.map(
-                p => [p.name, p.description, p.price]
-            )
-        )).catch(err => console.log("Error: ", err))
+        refreshPage();
     }, [])
 
   return (
